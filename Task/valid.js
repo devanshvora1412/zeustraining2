716af531bc324a27b73bbb9a371db1bd6d90d924
DevanshVoraZeus <devansh.vora@zeuslearning.com>
2024-06-05 15:32:52 +0530

    New Repository

---
+++ Task/valid.js
@@ -0,0 +1,9 @@
+function validateForm(uname,ucomments) {
+    var uname = document.forms["myForm"]["name"].value
+    var ucomments = document.forms["myForm"]["comments"].value
+    if (uname == "" || ucomments == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if 
+  }
